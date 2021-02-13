9c99e9a8e5cd945ebd1795bfcc0ffd4ceadf9622
Enthöfer Joshua <j.enthoefer@laudert.de>
2021-02-13 07:49:56 +0100

    First commit

---
+++ app/src/main/java/de/jo20046/probeklausur_neu/MainActivity.java
@@ -0,0 +1,14 @@
+package de.jo20046.probeklausur_neu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
